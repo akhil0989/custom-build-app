3709f3f72f65c9cba35adba0134fb91729db9ba2
akhil0989 <techakhil.09@gmail.com>
2025-04-14 18:09:49 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,17 @@
 // eslint.config.js (flat config style)
-module.exports = [
-  {
-    files: ['*.js'],
-    rules: {
-      'no-console': 'off',  // Allow console logs
-      'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-      // Add any other rules you prefer
-    },
+import { defineConfig } from 'eslint';
+
+export default defineConfig({
+  files: ['*.js'],  // Specify the files to which the config applies
+  env: {
+    node: true,
+    es2021: true,
   },
-];
+  parserOptions: {
+    ecmaVersion: 12, // Support for ES2021 features
+  },
+  rules: {
+    'no-console': 'off',  // Allow console logs
+    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+  },
+});
