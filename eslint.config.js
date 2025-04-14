d7afa78a54ffef176b8bfaefd8fc33b7fdfee8c8
akhil0989 <techakhil.09@gmail.com>
2025-04-14 18:08:11 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,9 @@ module.exports = [
   {
     files: ['*.js'],
     rules: {
-      'no-console': 'off',  // Allow console logs (remove this if not needed)
+      'no-console': 'off',  // Allow console logs
       'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-      // Add any other ESLint rules you prefer
+      // Add any other rules you prefer
     },
   },
 ];
