b89d05453a2b23fedd64e053b6a89afdb7d7055f
akhil0989 <techakhil.09@gmail.com>
2025-04-14 19:03:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Simple route
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-// We need to export the app instance for testing
-module.exports = app;
-
-app.listen(port, () => {
+// Start the server
+const server = app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+module.exports = { app, server };  // Export both app and server for testing purposes
